b2e769e8cf285ccb5cd32e5f9cd4934140062b5e
scgopi <scgopireddy@gmail.com>
2026-08-04 07:53:55 -0700

    graphcode 0.1.16

--- Casks/graphcode.rb
+++ Casks/graphcode.rb
@@ -1,6 +1,6 @@
 cask "graphcode" do
-  version "0.1.15"
-  sha256 "847f2bcbc063b902a400f8054b85a65cb888d0d87d85784bafd33cae7131a415"
+  version "0.1.16"
+  sha256 "cc1181d7a897288c4862d99d0e463fbf2ea719d47eccf924f75090dd1bb8c760"
 
   url "https://github.com/scgopi/GraphCode/releases/download/v#{version}/graphcode-macos-arm64.dmg",
       verified: "github.com/scgopi/GraphCode/"
