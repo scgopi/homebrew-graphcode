c86e5e26d82324e8c0d76b1b93cf2b947b221eaf
Sravani Pagidela <sravani2201@outlook.com>
2026-08-30 10:15:07 -0700

    graphcode@beta 0.1.57-beta3

--- Casks/graphcode@beta.rb
+++ Casks/graphcode@beta.rb
@@ -1,6 +1,6 @@
 cask "graphcode@beta" do
-  version "0.1.57-beta2"
-  sha256 "fbbb592c3ccfcde487df11bad19a2d32dc1772cd80a5aaa3db57b49d7adf5382"
+  version "0.1.57-beta3"
+  sha256 "de3d91e2111e96da0b951aae49f38cd97463d1091491b5fb413abbb4a206ba40"
 
   # No `v` in the path: betas are tagged bare (0.1.9-beta1) while releases carry
   # the prefix (v0.1.9), which is also why `make tap-bump CHANNEL=beta` keeps a
